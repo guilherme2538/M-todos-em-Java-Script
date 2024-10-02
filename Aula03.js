e8f7a1fee96e939bafa255c897790b50ff62e65d
guilherme2538 <126290619+guilherme2538@users.noreply.github.com>
2024-10-02 17:01:37 -0400

    Add files via upload

---
+++ Aula03.js
@@ -0,0 +1,14 @@
+// 1. Método push()
+let frutas = ['maçã', 'banana'];
+frutas.push('laranja');
+console.log(frutas); // Saída: ['maçã', 'banana', 'laranja']
+
+// 2. Método shift()
+let vegetais = ['cenoura', 'batata', 'brócolis'];
+vegetais.shift();
+console.log(vegetais); // Saída: ['batata', 'brócolis']
+
+// 3. Método sort()
+let numeros = [4, 2, 5, 1, 3];
+numeros.sort();
+console.log(numeros); // Saída: [1, 2, 3, 4, 5]
